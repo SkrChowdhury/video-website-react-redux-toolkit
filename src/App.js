4fe6b766015ad35957a636ebcc0d953930e93ad0
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2023-09-02 21:49:45 +0600

    all components and pages are set

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Home from './pages/Home'
 import React from 'react'
+import Video from './pages/Video'
 
 export default function App() {
   return (
-  <Home/>
+  <Video/>
   )
 }
---
+++ src/pages/Video.js
@@ -0,0 +1,30 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/navbar/Navbar";
+import React from "react";
+import RelatedVideoList from "../components/list/RelatedVideoList";
+import VideoDescription from "../components/description/VideoDescription";
+import VideoPlayer from "../components/description/Player";
+
+export default function Video() {
+  return (
+    <>
+      <Navbar />
+
+      <section class="pt-6 pb-20">
+        <div class="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
+          <div class="grid grid-cols-3 gap-2 lg:gap-8">
+            <div class="col-span-full w-full space-y-8 lg:col-span-2">
+              <VideoPlayer />
+
+              <VideoDescription />
+            </div>
+
+            <RelatedVideoList />
+          </div>
+        </div>
+      </section>
+
+      <Footer />
+    </>
+  );
+}
