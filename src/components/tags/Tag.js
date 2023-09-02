9679f0e397bd67dbd127f95f27fec3287ddaab47
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2023-09-02 20:32:27 +0600

    Video Grid and Items design added

---
+++ src/components/tags/Tag.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Tag() {
+  return (
+    <div class="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
+      react
+    </div>
+  );
+}
