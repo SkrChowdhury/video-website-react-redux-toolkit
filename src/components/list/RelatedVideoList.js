d088a59e2880625d667e7437435e53b4a49e24fc
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2023-09-23 16:13:13 +0600

    related videos feature implementation done

--- src/components/list/RelatedVideoList.js
+++ src/components/list/RelatedVideoList.js
@@ -1,13 +1,36 @@
-import React from 'react'
-import RelatedVideoListItem from "./RelatedVideoListItem"
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-export default function RelatedVideoList() {
+import Loading from "../Loading";
+import RelatedVideoListItem from "./RelatedVideoListItem";
+import { fetchRelatedVideos } from "../../features/relatedVideos/relatedVideosSlice";
+
+export default function RelatedVideoList({ currentVideoId, tags }) {
+  const dispatch = useDispatch();
+  const { relatedVideos, isLoading, isError, error } = useSelector(
+    (state) => state.relatedVideos
+  );
+
+  useEffect(() => {
+    dispatch(fetchRelatedVideos({ tags, id: currentVideoId }));
+  }, [dispatch, tags, currentVideoId]);
+
+  //decide what to render
+
+  let content = null;
+
+  if (isLoading) content = <Loading />;
+  if (!isLoading && isError)
+    content = <div className="col-span-12">{error}</div>;
+  if (!isLoading && !isError && relatedVideos?.length === 0)
+    content = <div className="col-span-12">No Related Videos Found</div>;
+  if (!isLoading && !isError && relatedVideos?.length > 0)
+    content = relatedVideos.map((video) => (
+      <RelatedVideoListItem key={video.id} video={video} />
+    ));
   return (
-    <div
-    className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto"
->
-
-<RelatedVideoListItem/>
-</div>
-  )
+    <div className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
+      {content}
+    </div>
+  );
 }
