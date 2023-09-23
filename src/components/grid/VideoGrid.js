b60e6741c504ad30164660ef2bf1d29d8b3c28c8
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2023-09-24 01:03:34 +0600

    final commit

--- src/components/grid/VideoGrid.js
+++ src/components/grid/VideoGrid.js
@@ -11,9 +11,11 @@ export default function VideoGrid() {
     (state) => state.videos
   );
 
+  const {tags,search} = useSelector(state => state.filter)
+
   useEffect(() => {
-    dispatch(fetchVideos());
-  }, [dispatch]);
+    dispatch(fetchVideos({tags,search}));
+  }, [dispatch, tags, search]);
 
   // decide what to render
 
